df8deecb2aefc6b6b64dd4009aec76c64e31e7f4
Brian Cottingham <spiffytech@gmail.com>
2013-01-01 17:54:27 -0500

    Fixed date calculation bug that made all checks not work in the first week of the month. Added caching.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var cons = require("consolidate");
 
 var express = require("express");
@@ -12,8 +14,16 @@ app.use(express.static(__dirname + "/static"));
 
 var step = require("step");
 
+var cache_age = 1000 * 60 * 30;
+var LRU = require("lru-cache");
+var cache = LRU({
+    max: 1024 * 1024 * 5,
+    length: function(val) {return JSON.stringify(val).length;},
+    maxAge: cache_age
+});
+
 var Requester = require("requester");
-requester = new Requester();
+var requester = new Requester();
 
 var _s = require("underscore.string");
 
@@ -59,8 +69,8 @@ app.get("/feed.xml", function(req, res) {
     });
 });
 
-do_stuff = function(req, callback) {
-    var day = req.query.day || new Date().getDay();
+var do_stuff = function(req, callback) {
+    var day = req.query.day || new Date().getUTCDay();
     var threshold = req.query.threshold || 25;
     console.log("Day: " + date_format_rss(new Date(calc_ts_range(day).end)));
     console.log("Threshold: " + threshold);
@@ -71,7 +81,7 @@ do_stuff = function(req, callback) {
     });
 }
 
-get_data = function(day, start_index, callback, posts) {
+var get_data = function(day, start_index, callback, posts) {
     var ts_range = calc_ts_range(day);
     var limit = 100;
     var query_str = "http://api.thriftdb.com/api.hnsearch.com/items/_search?filter[fields][create_ts]=[%s TO %s]&pretty_print=true&sortby=points desc&limit=%d&start=%d";
@@ -79,12 +89,19 @@ get_data = function(day, start_index, callback, posts) {
         posts = [];
     }
 
+    var cached_posts = cache.get("day-" + day);
+    if(cached_posts) {
+        console.log("Cache hit: day " + day);
+        callback(cached_posts);
+        return;
+    }
+
     requester.get(
         _s.sprintf(query_str, ts_range.start, ts_range.end, limit, start_index),
         function(body) {
             var resp = JSON.parse(body);
 
-            for(result in resp.results) {
+            for(var result in resp.results) {
                 if(!resp.results.hasOwnProperty(result)) continue;
                 posts.push(resp.results[result]);
             }
@@ -99,13 +116,18 @@ get_data = function(day, start_index, callback, posts) {
                     posts[post].item.rss_date = date_format_rss(d);
                     not_stupid_posts.push(posts[post].item);
                 }
+                console.log("Cache miss: day " + day);
+                if(within_cache_intelval(day)) {
+                    console.log("Data size: " + (JSON.stringify(not_stupid_posts).length / 1024) / 1024);
+                    cache.set("day-" + day, not_stupid_posts);
+                }
                 callback(not_stupid_posts);
             }
         }
     );
 };
 
-validate_inputs = function(req, res) {
+var validate_inputs = function(req, res) {
     if(req.query.threshold) {
         if(req.query.threshold <= 0 || req.query.threshold > 1000) {
             res.send(404, {error: "Threshold out of range"});
@@ -121,7 +143,7 @@ validate_inputs = function(req, res) {
     }
 }
 
-date_format_rss = function(d) {
+var date_format_rss = function(d) {
     var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     return _s.sprintf("%s, %02d %s %d %02d:%02d:%02d UTC", 
@@ -135,7 +157,7 @@ date_format_rss = function(d) {
     );
 }
 
-percentile_filter = function(posts, threshold) {
+var percentile_filter = function(posts, threshold) {
     var culled_post_count = Math.round(posts.length * (1-threshold) + .5);  // Not sure what the .5 is for, but that's what Wikipedia says should be in there
     var culled_posts = posts.slice(0, culled_post_count);
     console.log(calc_point_range(culled_posts));
@@ -144,7 +166,7 @@ percentile_filter = function(posts, threshold) {
 }
 
 
-calc_point_range = function(posts) {
+var calc_point_range = function(posts) {
     return {
         min: + posts[posts.length-1].points,
         max: posts[0].points
@@ -152,17 +174,31 @@ calc_point_range = function(posts) {
 }
 
 
-calc_ts_range = function(day) {
+var within_cache_intelval = function(day) {
+    var now = new Date();
+    var start = new Date();
+    start.setUTCHours(0);
+    start.setUTCMinutes(0);
+    start.setUTCMilliseconds(0);
+    start.setTime(start.getTime() + 1000 * 60 * 60 * 24);  // Forces checks for e.g., day 2 while on day 2, to check /next week's/ day 2 and not the already-past today-at-midnight
+    while(start.getUTCDay() != day) {
+        start.setTime(start.getTime() + 1000 * 60 * 60 * 24);
+    }
+    return (start - now) > cache_age;
+}
+
+
+var calc_ts_range = function(day) {
     var end = new Date();
     end.setUTCHours(0);
     end.setUTCMinutes(0);
     end.setUTCMilliseconds(0);
-    while(end.getDay() != day) {
-        end.setDate(end.getDate() - 1);
+    while(end.getUTCDay() != day) {
+        end.setTime(end.getTime() - 1000 * 60 * 60 * 24);
     }
 
     var start = new Date();
-    start.setDate(end.getDate() - 7);
+    start.setTime(end.getTime() - 1000 * 60 * 60 * 24 * 7);
 
     return {
         start: start.toISOString(),
